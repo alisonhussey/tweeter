cda8844e56c9421ae51aa3b05f0812587c1ef7d4
alisonhussey <alisonhussey@gmail.com>
2020-12-16 23:57:02 +0000

    added function and css for character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#tweet-text").on("input", function(event) {
+    const charsLeft = 140 - $(this).val().length;
+    const counterNode = $(this).closest("form").find(".counter");
+    counterNode.val(charsLeft);
+    if (charsLeft < 0) {
+      counterNode.addClass("negative");
+    } else {
+      counterNode.removeClass("negative");
+    }
+  });
+});
