9a7ccf453c4a2b2666d74e1fd10fb5ffbea9fc58
alisonhussey <alisonhussey@gmail.com>
2020-12-18 20:18:16 +0000

    added validations to subit handler for if tweet is empty or too long

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,45 +1,79 @@
-
-$(document).ready(function() {
-
-const tweetData = {
-    "user": {
-      "name": "Alison",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
- }
-
 const createTweetElement = function(tweet) {
-  console.log(tweet.user)
-  const $tweet = `<article class="tweet">
-  <header>
-    <span><img src=${tweet.user.avatars}<span>${tweet.user.name}</span></span>
-    <span class="username">${tweet.user.handle}</span>
-  </header>
-  <div class="aboveBorder">${tweet.content.text}</div>
-  <footer>
-    <span class="timestamp">${tweet["created_at"]}</span>
-    <span class="actions">
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="fas fa-heart"></i>
-    </span>
-  </footer>
-</article>`;
+  const $tweet = `
+  <article class="tweet">
+    <header>
+      <span><img src=${tweet.user.avatars}<span>${tweet.user.name}</span></span>
+      <span class="username">${tweet.user.handle}</span>
+    </header>
+    <div class="aboveBorder">${tweet.content.text}</div>
+    <footer>
+      <span class="timestamp">${tweet["created_at"]}</span>
+      <span class="actions">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </span>
+    </footer>
+  </article>`;
  
   return $tweet;
 }
 
+const renderTweets = function(tweets) {
+  for (let tweet of tweets) {
+    $tweet = createTweetElement(tweet)
+    $('#tweets-container').append($tweet);
+    //empty out tweets-container
+  } 
+}
 
 
-const $tweet = createTweetElement(tweetData);
+const submitWithAjax = function() {
+  const $form = $('form')
+$form.submit(function (event) {
+  console.log("form")
+  event.preventDefault()
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').html("").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  const textarea = $(this).find("#tweet-text")
+
+  const text = textarea.val();
+  const length = textarea.val().length;
+
+  if (text === "" || text === null) {
+    alert("Tweet Empty");
+  } else if (length > 140) {
+    alert("Tweet must be under 140 characters");
+  } else {
+    $.ajax('/tweets/', {method: 'POST', data: $(this).serialize()})
+  .then(function() {
+    console.log("Success");
+  })
+  }
+})
+}
+
+const loadTweets = function() {
+  $.ajax({
+    method: 'GET',
+    url: '/tweets/',
+  })
+  .then((result) => {
+    console.log(result);
+    renderTweets(result);
+  })
+  // .catch((err) => console.log(err));
+
+}
+
+
+$(document).ready(function() {
+
+
+
+loadTweets();
+
+submitWithAjax();
+
+
 
 });
