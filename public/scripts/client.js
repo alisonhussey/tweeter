1f39517e350f4f0bf95ebe7f742e2e502e6a7988
alisonhussey <alisonhussey@gmail.com>
2020-12-20 18:47:25 +0000

    reset character counter on submission and fixed word overflow

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,12 @@
 const createTweetElement = function(tweet) {
+
+  // const tweetDate = ${tweet.created_at};
+  // const today = Date.now();
+
+  // const diff = (today.getSeconds() - tweetDate.getSeconds());
+  // const timeSince = Math.floor(diff - 24 * 60 * 60 * 1000)
+
+
   const $tweet = `
   <article class="tweet">
     <header class="tweet-header">
@@ -10,7 +18,7 @@ const createTweetElement = function(tweet) {
     </header>
     <div class="aboveBorder">${escape(tweet.content.text)}</div>
     <footer>
-      <span class="timestamp">${tweet["created_at"]}</span>
+      <span class="timestamp">${tweet.created_at}</span>
       <span class="actions">
         <i class="fas fa-flag"></i>
         <i class="fas fa-retweet"></i>
@@ -20,75 +28,68 @@ const createTweetElement = function(tweet) {
   </article>`;
  
   return $tweet;
-}
+};
 
 const escape =  function(str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
-}
+};
 
 const renderTweets = function(tweets) {
   $('#tweets-container').empty();
   for (let tweet of tweets) {
-    $tweet = createTweetElement(tweet)
+    $tweet = createTweetElement(tweet);
     $('#tweets-container').prepend($tweet);
-  } 
-}
+  }
+};
 
 
 const submitWithAjax = function() {
-  const $form = $("form")
-$form.submit(function (event) {
-  console.log("form")
-  event.preventDefault()
+  const $form = $("form");
+  $form.submit(function(event) {
+    console.log("form");
+    event.preventDefault();
 
-  const textarea = $(this).find("#tweet-text")
-  const text = textarea.val();
-  const length = textarea.val().length;
-  $(".errorempty").slideUp(1000)
-  $(".errorlong").slideUp(1000)
+    const textarea = $(this).find("#tweet-text");
+    const text = textarea.val();
+    const length = textarea.val().length;
+    // const counter = $(this).find(".counter");
 
-  if (text === "" || text === null) {
-    $(".errorempty").slideDown(1000);
-  } else if (length > 140) {
-    $(".errorlong").slideDown(1000);
-  } else {
-    $.ajax('/tweets/', {method: 'POST', data: $(this).serialize()})
-  .then(function() {
-    console.log("Success");
-    loadTweets()
-    $('#tweet-text').val('');
-  })
-  }
-})
-}
+    $(".errorempty").slideUp(1000);
+    $(".errorlong").slideUp(1000);
+
+    if (text === "" || text === null) {
+      $(".errorempty").slideDown(1000);
+    } else if (length > 140) {
+      $(".errorlong").slideDown(1000);
+    } else {
+      $.ajax('/tweets/', {method: 'POST', data: $(this).serialize()})
+        .then(function() {
+          console.log("Success");
+          loadTweets();
+          $('#tweet-text').val("");
+          $(".counter").val("140");
+        });
+    }
+  });
+};
 
 const loadTweets = function() {
   $.ajax({
     method: 'GET',
     url: '/tweets/',
   })
-  .then((result) => {
-    console.log(result);
-    renderTweets(result);
-
-  })
-  // .catch((err) => console.log(err));
-
-}
+    .then((result) => {
+      console.log(result);
+      renderTweets(result);
+    });
+};
 
 
 
 
 $(document).ready(function() {
-
-
-
-loadTweets();
-
-submitWithAjax();
-
-
-
+  loadTweets();
+  submitWithAjax();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $("#tweet-text").on("input", function(event) {
+    $('#tweet-text').empty();
     const charsLeft = 140 - $(this).val().length;
     const counterNode = $(this).closest("form").find(".counter");
     counterNode.val(charsLeft);
